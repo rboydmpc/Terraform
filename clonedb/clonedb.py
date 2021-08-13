f5d8c31e9b4ab096a65b709739167f665359d0c2
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2021-08-13 17:18:30 -0400

    Update clonedb.py

--- clonedb/clonedb.py
+++ clonedb/clonedb.py
@@ -1,11 +1,33 @@
 import boto3
-import datetime
-import logging
-import os
+import time
+#import logging
+#import os
+#import sys
 from operator import itemgetter
 from pprint import pprint
 
-# set resource and region
+# grab current time for role and DB Snapshot names
+time_stamp = time.strftime("%Y%m%d-%H%M%S")
+#logger = logging.getLogger(name=__name__)
+#env_level = os.environ.get("LOG_LEVEL")
+#log_level = logging.INFO if not env_level else env_level
+#logger.setLevel(log_level)
+
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+def assume_role(role_arn, region):
+    """Grabs temporary keys of assumed roles from ARN
+
+    Args:
+        role_arn: ARN of the role you want to assume
+        region: AWS Region based on Environment selected input_Environemnt
+
+    Returns:
+        temp_key: Temporary key for assumed role
+        temp_secret: Temporary secret key for assumed role
+    """
+
+    # set resource and region
     client = boto3.client(
         'sts',
         region_name = region
@@ -22,21 +44,8 @@ from pprint import pprint
 
     return temp_key, temp_secret, temp_token
 
-def rds_service(temp_key, temp_secret, temp_token, task, db_name, region):
-    """    Sends Boto3 tasks based on task input
 
-    Args:
-        temp_key: Temporary key from assume_role function
-        temp_secret: Temporary secret ket from assume_role function
-        temp_token: Temporary session token from the assume_role function
-        task: Input task to run corresponding Boto3 task
-        db_name: Name of RDS to run task against
-        region: AWS Region based on Environment selected input_Environemnt
-
-    Returns:
-        AWS JSON message
-    """    
-
+def rds_client(temp_key, temp_secret, temp_token, db_name, region):
     
     # set resource and region
     client = boto3.client(
@@ -47,41 +56,73 @@ def rds_service(temp_key, temp_secret, temp_token, task, db_name, region):
         aws_session_token = temp_token
         )
     
-    list_snapshots = client.describe_db_snapshots(
-                    DBInstanceIdentifier=db_name
-                )
+    return client
 
     #sorted_keys = sorted(list_snapshots['DBSnapshots'], key=itemgetter('SnapshotCreateTime'), reverse=True)
     #snapshot_arn = sorted_keys[0]['DBSnapshotArn']
     #dbsnapshotidentifier = sorted_keys[0]['DBSnapshotIdentifier']
     #pprint(dbsnapshotidentifier)
-    pprint(list_snapshots)
+    #pprint(list_snapshots)
     
- def copy_latest_snapshot(db_identifier):
-    timestamp = '{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now())
-    snapshot_copy = f"snapshot-copy{timestamp}"
-    logger.info(f"Find latest snapshot of: {db_identifier}")
-    response = rds.describe_db_snapshots(DBInstanceIdentifier=db_identifier, SnapshotType='automated')
+def copy_latest_snapshot(db_identifier, client, waiter):
+    #timestamp = '{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now())
+    snapshot_copy = f"snapshot-copy{time_stamp}"
+    #logger.info(f"Find latest snapshot of: {db_identifier}")
+    response = client.describe_db_snapshots(DBInstanceIdentifier=db_identifier, SnapshotType='automated')
     sorted_keys = sorted(response['DBSnapshots'], key=itemgetter('SnapshotCreateTime'), reverse=True)
     snapshot_id = sorted_keys[0]['DBSnapshotIdentifier']
-    logger.info(f"Wait for snapshot: {snapshot_id}")
+    #logger.info(f"Wait for snapshot: {snapshot_id}")
     waiter.wait(
         DBInstanceIdentifier=db_identifier,
         DBSnapshotIdentifier=snapshot_id,
         WaiterConfig={'Delay': 5, 'MaxAttempts': 12}
     )
-    logger.info(f"Create snapshot copy: {snapshot_copy} of {db_identifier}")
-    rds.copy_db_snapshot(SourceDBSnapshotIdentifier=snapshot_id,
+    #logger.info(f"Create snapshot copy: {snapshot_copy} of {db_identifier}")
+    client.copy_db_snapshot(SourceDBSnapshotIdentifier=snapshot_id,
                          TargetDBSnapshotIdentifier=snapshot_copy)
+    #pprint(logger.info)
     return snapshot_copy
+
+def share_copy_snapshot(db_identifier, snapshot_id, nonprodaccount, client, waiter):
+        #max_wait = int(os.environ.get("MAX_WAIT"))
+        #max_att = int(max_wait / 5)
+        #logger.info(f"Wait for snapshot: {snapshot_id}")
+        waiter.wait(
+            DBInstanceIdentifier=db_identifier,
+            DBSnapshotIdentifier=snapshot_id,
+            WaiterConfig={'Delay': 10, 'MaxAttempts': 100}
+        )
+        #shared_account = os.environ.get("SHARED_ACCOUNT")
+        #logger.info(f"Share snapshot: {snapshot_id} of {db_identifier}")
+        client.modify_db_snapshot_attribute(
+            DBSnapshotIdentifier=snapshot_id,
+            AttributeName="restore",
+            ValuesToAdd=[nonprodaccount]
+        )
+
+def get_latest_snapshot_arn(client):
+    response = client.describe_db_snapshots(
+        IncludeShared=True,
+        SnapshotType="shared",
+    )
+    if not response['DBSnapshots']:
+        return
+    sorted_keys = sorted(response['DBSnapshots'], key=itemgetter('SnapshotCreateTime'), reverse=True)
+    snapshot_arn = sorted_keys[0]['DBSnapshotArn']
+    return snapshot_arn
+    pprint(response)
+    pprint(snapshot_arn)
+
     
 def main():
 
     # grab inputs from morpheus dictory
-    input_arn = 'arn:aws-us-gov:iam::147884775654:role/CICD-rds-role' #morpheus['customOptions']['RDSRoleARN']
-    input_task = 'test' #morpheus['customOptions']['RDSTask']
+    input_prod_arn = 'arn:aws-us-gov:iam::147884775654:role/AWSCrossAccountRoleForTerraform'  #morpheus['customOptions']['RDSRoleARN']
+    input_nonprod_arn = 'arn:aws-us-gov:iam::940330411695:role/AWSCrossAccountRoleForTerraform'  #morpheus['customOptions']['RDSRoleARN']
+    #input_task = 'test' #morpheus['customOptions']['RDSTask']
     input_db_name = 'boyd881211'  #morpheus['customOptions']['RDSInstance']
     input_environemnt = 'Prod' #morpheus['customOptions']['RDSENV']
+    input_nonprod_accout = '940330411695'
     
     # Check instance environment
     if input_environemnt == "DR":
@@ -89,16 +130,31 @@ def main():
     else:
       region = 'us-gov-west-1'
 
-
-    # grab temp keys
-    temp_key, temp_secret, temp_token = assume_role(input_arn, region)
+    ####From Prod AWS
+    # grab temp keys for Prod
+    temp_key, temp_secret, temp_token = assume_role(input_nonprod_arn, region)
     #pprint(temp_key)
     #pprint(temp_secret)
     #pprint(temp_token)
 
 
-    # send task to db with temp keys
-    rds_service(temp_key, temp_secret, temp_token, input_task, input_db_name, region)
+    # connect rds client
+    client = rds_client(temp_key, temp_secret, temp_token, input_db_name, region)
+    waiter = client.get_waiter('db_snapshot_available')
+    
+    #list snapshots 
+    list_snapshots = client.describe_db_snapshots(
+                    DBInstanceIdentifier=input_db_name
+                )
+    
+    #copy DB snapshot
+    snapshot_copy = copy_latest_snapshot(input_db_name, client, waiter)
+    share_snapshot = share_copy_snapshot(input_db_name, snapshot_copy, input_nonprod_accout, client, waiter)
+    get_latest_snapshot_arn(client)
+    #pprint(return_snapshot)
+    #pprint(return_snapshot)
+    pprint(snapshot_copy)
+    #pprint(logger.info)
+    
 if __name__ == "__main__":
     main()
-   
