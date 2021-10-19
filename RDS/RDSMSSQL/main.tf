da4702da1e8ffef0f276eee09a77083d4f191654
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2021-10-19 08:44:37 -0400

    Update main.tf

--- RDS/RDSMSSQL/main.tf
+++ RDS/RDSMSSQL/main.tf
@@ -5,7 +5,6 @@ module "mod_rds" {
     access_key              = var.access_key
     secret_key              = var.secret_key
     instance_class          = var.instance_class
-    client                  = var.client
     environment             = var.environment
     region                  = var.region
     multi_az                = var.multi_az
@@ -22,26 +21,6 @@ module "mod_rds" {
     storage_type            = var.storage_type
     iops                    = var.iops
     character_set_name      = var.character_set_name
-    workspaces_cidr         = var.workspaces_cidr
     apply_immediately       = var.apply_immediately
-    snapshot_id             = var.snapshot_id
 }
 
-#module "mod_secretsmanager" {
-#    source = "./modules/SecretsManager/"
-    
-#    #inputs:
-#    soc_access_key          = var.soc_access_key
-#    soc_secret_key          = var.soc_secret_key
-#    secret_name             = format("%s/%s/RDS/%s", var.client, var.account_name, var.db_name)
-#    secret_value_dbname     = lower(var.db_name)
-#    secret_value_dbendpoint = module.mod_rds.new_db_instance_endpoint
-#    secret_value_dbarn      = module.mod_rds.new_db_instance_arn
-#    secret_value_dbusername = module.mod_rds.new_db_instance_username
-#    secret_value_dbpassword = module.mod_rds.new_db_instance_password
-#    db_port                 = var.db_port
-#    account_name            = var.account_name
-#    engine_name             = var.engine_name
-#    secret_cmk_id           = var.secret_cmk_id
-#    cicd_role_arn           = var.cicd_role_arn
-#}
