1975a33cddbe04b39e1a74230fb7a93d8d70ef97
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2021-10-28 10:14:25 -0400

    sdsda

--- EKSnew/vpc.tf
+++ EKSnew/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "education-eks-test1"
 }
 
 resource "random_string" "suffix" {
