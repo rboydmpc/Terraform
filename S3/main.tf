a180ee786b4e4da1ec860fcdea7dbaff088bc207
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2021-12-08 14:43:22 -0500

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -1,3 +1,14 @@
+terraform {
+
+  required_version = ">= 0.12.31"
+  required_providers {
+
+    aws = ">= 3.64"
+
+  }
+}
+
+
 provider "aws" {
     region = "us-east-1"
     access_key = var.access_key
