90e5500feafa2553e0a8f1b3f273db4dd10d72e4
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2022-03-01 11:34:29 -0500

    ffdg

--- WAF/prod/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  prod_external_alb = "arn:aws-us-gov:elasticloadbalancing:us-gov-west-1:933536758957:loadbalancer/app/prod-ginniemae-gov-ALB/b207cb8b2dff40b0"
-}
--- WAF/prod/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  default     = "us-east-1"
-  description = "AWS Region"
-  type        = string 
-}
-variable "access_key" {}
-variable "secret_key" {}
