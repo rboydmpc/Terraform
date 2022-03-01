34da16c93cc0fffe6d8e94f24938a8935d9213c3
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2022-03-01 11:20:05 -0500

    Update main.tf

--- S3New/main.tf
+++ S3New/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "demos3" {
     bucket = lower(var.bucket_name)
     acl = var.acl_value 
-    
+    force_destroy = true
     tags = {
     Name = var.bucket_name 
     Date = timestamp()
